7fd816840feb9042eb965e7f240111efd5a32b40
ayesha-rahman37 <ayesha30089@gmail.com>
2026-03-16 21:16:10 +0600

    Update login/signup system with automated generate unique Id for use

--- MedEdu/urls.py
+++ MedEdu/urls.py
@@ -3,12 +3,10 @@ from . import views
 
 urlpatterns = [
 
-    path("", views.home, name="home"),
-
-    path("signup/", views.signup_view, name="signup"),
-
-    path("login/", views.login_view, name="login"),
-
-    path("logout/", views.logout_view, name="logout"),
+    path('', views.home, name='home'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
 
 ]
--- MedEdu/views.py
+++ MedEdu/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
 from .forms import SignupForm
 from .models import User
 
 
 def home(request):
-
     return render(request, "home.html")
 
 
+# SIGNUP
 def signup_view(request):
 
     form = SignupForm(request.POST or None)
@@ -16,43 +17,45 @@ def signup_view(request):
     if form.is_valid():
 
         user = form.save(commit=False)
-
         user.set_password(form.cleaned_data['password'])
-
         user.save()
 
-        return redirect("login")
+        return render(request, "signup_success.html", {
+            "mededu_id": user.mededu_id
+        })
 
     return render(request, "signup.html", {"form": form})
 
 
+# LOGIN
 def login_view(request):
 
     if request.method == "POST":
 
         mededu_id = request.POST.get("mededu_id")
-
         password = request.POST.get("password")
 
         try:
-
             user = User.objects.get(mededu_id=mededu_id)
 
             if user.check_password(password):
 
                 login(request, user)
-
-                return redirect("home")
+                return redirect("profile")
 
         except User.DoesNotExist:
-
-            pass
+            return render(request, "login.html", {
+                "error": "Invalid MedEdu ID or Password"
+            })
 
     return render(request, "login.html")
 
 
 def logout_view(request):
-
     logout(request)
-
     return redirect("home")
+
+
+@login_required
+def profile_view(request):
+    return render(request, "profile.html")
