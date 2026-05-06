b2535d670d1c5db96b9497293688fefd07c7f846
ayesha-rahman37 <ayesha30089@gmail.com>
2026-05-06 20:20:15 +0600

    Add OT management system for intern doctor, student & doctor

--- MedEdu/models.py
+++ MedEdu/models.py
@@ -273,7 +273,8 @@ class ClassSchedule(models.Model):
 
     def __str__(self):
         return f"{self.faculty} - {self.subject} ({self.date})"
-    
+
+# ================= ATTENDANCE =================    
 class Attendance(models.Model):
     
     STATUS = (
@@ -289,3 +290,31 @@ class Attendance(models.Model):
 
     def __str__(self):
         return f"{self.student} - {self.status}"
+    
+# ================= OT MANAGEMENT =================
+class OperationSchedule(models.Model):
+
+    doctor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'doctor'}
+    )
+
+    patient_name = models.CharField(max_length=200)
+    disease = models.CharField(max_length=200)
+
+    room = models.CharField(max_length=100)
+
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    # students + interns who can attend
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="ot_classes",
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.patient_name} - {self.doctor}"
